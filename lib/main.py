60cef44b4bd83540d872e62b03c9fb5c52c9af3b
Daazed Mcfarland <daazedjmcfarland@gmail.com>
2025-04-24 09:53:10 -0700

    class name and desc json

---
+++ lib/main.py
@@ -0,0 +1,40 @@
+import json
+import os
+from os.path import isdir, isfile, join
+
+component_path = "./components/"
+component_sub = os.listdir(component_path)
+component_dirs = [
+    join(component_path, name)
+    for name in component_sub
+    if isdir(join(component_path, name))
+]
+
+component_contents = []
+for dir_path in component_dirs:
+    dir_content = os.listdir(dir_path)
+    for name in dir_content:
+        name_path = join(dir_path, name)
+        if isfile(name_path):
+            component_contents.append(open(name_path, "r").readlines())
+
+components = []
+class_name_pattern = "- class:"
+class_desc_pattern = "desc:"
+for component in component_contents:
+    for i, line in enumerate(component):
+        line = str(line)
+        if class_name_pattern in line:
+            start = line.index(":") + 2
+            name = line[start:]
+            if name[0] == "'":
+                name = name[1:-2]
+            name = name.strip()
+            desc_line = component[i + 1]
+            if class_desc_pattern in desc_line:
+                start = desc_line.index(":") + 2
+                desc = desc_line[start:].strip()
+                components.append({name: {"desc": desc}})
+
+with open("components.json", "w") as file:
+    json.dump(components, file, indent=4)
