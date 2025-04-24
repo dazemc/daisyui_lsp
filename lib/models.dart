1b858f5eeb2a8f3b6f92d492ad1f9d42c3013d8b
Daazed Mcfarland <daazedjmcfarland@gmail.com>
2025-04-24 10:38:29 -0700

    move method into the right place

--- lib/models.dart
+++ lib/models.dart
@@ -4,14 +4,14 @@ class DaisyuiComponent {
   final String label;
   final String detail;
   const DaisyuiComponent({required this.label, required this.detail});
-}
 
-CompletionItem toCompletionItem(DaisyuiComponent component) {
-  return CompletionItem(
-    label: component.label,
-    kind: CompletionItemKind.Text,
-    detail: component.detail,
-    insertText: component.label,
-    insertTextFormat: InsertTextFormat.PlainText,
-  );
+  CompletionItem toCompletionItem(DaisyuiComponent component) {
+    return CompletionItem(
+      label: component.label,
+      kind: CompletionItemKind.Text,
+      detail: component.detail,
+      insertText: component.label,
+      insertTextFormat: InsertTextFormat.PlainText,
+    );
+  }
 }
